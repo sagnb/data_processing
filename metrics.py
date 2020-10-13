a5a94854d60bfecb52d335731c4992c63f2fb6e7
Guilherme Neri Bustamante Sá <guinbsa@gmail.com>
2020-10-13 20:09:48 -0300

    metrics

--- metrics.py
+++ metrics.py
@@ -12,14 +12,24 @@ def f_measure(y_test, y, beta=1):
   tn = 0.0 # true neg
   fn = 0.0 # false neg
   for i in range(len(y)):
-    if y_test[i] == 1 and y[i] == 1:
+    if y_test[i] == 1.0 and y[i] == 1.0:
       tp += 1.0
-    elif y_test[i] == 1 and y[i] == 0:
+    elif y_test[i] == 1.0 and y[i] == 0.0:
       fp += 1.0
-    elif y_test[i] == 0 and y[i] == 0:
+    elif y_test[i] == 0.0 and y[i] == 0.0:
       tn += 1.0
-    elif y_test[i] == 0 and y[i] == 1:
+    elif y_test[i] == 0.0 and y[i] == 1.0:
       fn += 1.0
-  precision = tp / (tp + fp)
-  recall = tp / (tp + fn)
-  return (1.0 + (beta ** 2.0)) * ((precision * recall) / (((beta ** 2.0) * precision) + recall))
+  if (tp + fp) != 0.0:
+    precision = tp / (tp + fp)
+  else:
+    precision = 0.0
+  if (tp + fn) != 0.0:
+    recall = tp / (tp + fn)
+  else:
+    recall = 0.0
+  if (((beta ** 2.0) * precision) + recall) != 0:
+    resultado = (1.0 + (beta ** 2.0)) * ((precision * recall) / (((beta ** 2.0) * precision) + recall))
+  else:
+    resultado = 0.0
+  return resultado
